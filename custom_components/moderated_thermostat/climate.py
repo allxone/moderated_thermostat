614b9a789000b9fc1737247b4564b33f71161682
Stefano Dell'Orto <7570447+allxone@users.noreply.github.com>
2025-06-29 19:49:06 +0000

    lint

--- custom_components/moderated_thermostat/climate.py
+++ custom_components/moderated_thermostat/climate.py
@@ -6,6 +6,7 @@ Extends the GenericThermostat to moderate temperature based on humidity limits.
 
 import logging
 import math
+from collections.abc import Mapping
 from datetime import datetime, timedelta
 from typing import Any
 
@@ -15,9 +16,26 @@ from homeassistant.components.climate.const import (
     HVACMode,
 )
 from homeassistant.components.generic_thermostat.climate import (
+    CONF_INITIAL_HVAC_MODE,
+    CONF_KEEP_ALIVE,
+    CONF_PRECISION,
+    CONF_TARGET_TEMP,
+    CONF_TEMP_STEP,
     GenericThermostat,
 )
+from homeassistant.components.generic_thermostat.const import (
+    CONF_AC_MODE,
+    CONF_COLD_TOLERANCE,
+    CONF_HEATER,
+    CONF_HOT_TOLERANCE,
+    CONF_MAX_TEMP,
+    CONF_MIN_DUR,
+    CONF_MIN_TEMP,
+    CONF_PRESETS,
+    CONF_SENSOR,
+)
 from homeassistant.const import (
+    CONF_NAME,
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
     UnitOfTemperature,
@@ -29,9 +47,74 @@ from homeassistant.core import (
     State,
     callback,
 )
+from homeassistant.helpers.entity_platform import (
+    AddConfigEntryEntitiesCallback,
+    AddEntitiesCallback,
+)
+from homeassistant.helpers.event import (
+    async_track_state_change_event,
+)
+
+from .const import CONF_LIMIT_HUM, CONF_SENSOR_HUM
 
 _LOGGER = logging.getLogger(__name__)
 DEFAULT_NAME = "Moderated Thermostat"
+
+
+async def _async_setup_config(
+    hass: HomeAssistant,
+    config: Mapping[str, Any],
+    unique_id: str | None,
+    async_add_entities: AddEntitiesCallback | AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up the moderated thermostat platform."""
+    name: str = config[CONF_NAME]
+    heater_entity_id: str = config[CONF_HEATER]
+    sensor_entity_id: str = config[CONF_SENSOR]
+    sensor_hum_entity_id: str = config[CONF_SENSOR_HUM]
+    min_temp: float | None = config.get(CONF_MIN_TEMP)
+    max_temp: float | None = config.get(CONF_MAX_TEMP)
+    target_temp: float | None = config.get(CONF_TARGET_TEMP)
+    limit_hum: float | None = config.get(CONF_LIMIT_HUM)
+    ac_mode: bool | None = config.get(CONF_AC_MODE)
+    min_cycle_duration: timedelta | None = config.get(CONF_MIN_DUR)
+    cold_tolerance: float = config[CONF_COLD_TOLERANCE]
+    hot_tolerance: float = config[CONF_HOT_TOLERANCE]
+    keep_alive: timedelta | None = config.get(CONF_KEEP_ALIVE)
+    initial_hvac_mode: HVACMode | None = config.get(CONF_INITIAL_HVAC_MODE)
+    presets: dict[str, float] = {
+        key: config[value] for key, value in CONF_PRESETS.items() if value in config
+    }
+    precision: float | None = config.get(CONF_PRECISION)
+    target_temperature_step: float | None = config.get(CONF_TEMP_STEP)
+    unit = hass.config.units.temperature_unit
+
+    async_add_entities(
+        [
+            ModeratedThermostat(
+                hass,
+                name,
+                heater_entity_id,
+                sensor_entity_id,
+                sensor_hum_entity_id,
+                min_temp,
+                max_temp,
+                target_temp,
+                limit_hum,
+                ac_mode,
+                min_cycle_duration,
+                cold_tolerance,
+                hot_tolerance,
+                keep_alive,
+                initial_hvac_mode,
+                presets,
+                precision,
+                target_temperature_step,
+                unit,
+                unique_id,
+            )
+        ]
+    )
 
 
 class ModeratedThermostat(GenericThermostat):
@@ -48,7 +131,7 @@ class ModeratedThermostat(GenericThermostat):
 
     _attr_should_poll = False
 
-    def __init__(
+    def __init__(  # noqa: PLR0913
         self,
         hass: HomeAssistant,
         name: str,
@@ -59,7 +142,7 @@ class ModeratedThermostat(GenericThermostat):
         max_temp: float | None,
         target_temp: float | None,
         limit_hum: float | None,
-        ac_mode: bool | None,
+        ac_mode: bool | None,  # noqa: FBT001
         min_cycle_duration: timedelta | None,
         cold_tolerance: float,
         hot_tolerance: float,
@@ -218,7 +301,9 @@ class ModeratedThermostat(GenericThermostat):
         self._target_hum = predicted_hum
 
     async def _async_control_heating(
-        self, time: datetime | None = None, force: bool = False
+        self,
+        time: datetime | None = None,
+        force: bool = False,  # noqa: FBT001, FBT002
     ) -> None:
         """Check if we need to turn heating on or off."""
         # Inject target temperature moderation into the control logic
@@ -283,3 +368,14 @@ class ModeratedThermostat(GenericThermostat):
             self._cur_hum = cur_hum
         except ValueError:
             _LOGGER.exception("Unable to update from sensor: %s")
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added."""
+        await super().async_added_to_hass()
+
+        # Add humidity listener
+        self.async_on_remove(
+            async_track_state_change_event(
+                self.hass, [self.sensor_hum_entity_id], self._async_sensor_hum_changed
+            )
+        )
--- custom_components/moderated_thermostat/const.py
+++ custom_components/moderated_thermostat/const.py
@@ -5,7 +5,6 @@ from homeassistant.const import Platform
 DOMAIN = "moderated_thermostat"
 PLATFORMS = [Platform.CLIMATE]
 
-CONF_HEATER = "heater"
 CONF_SENSOR_HUM = "target_sensor_hum"
 CONF_LIMIT_HUM = "limit_hum"
 DEFAULT_LIMIT_HUM = 60.0
